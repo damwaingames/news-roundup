1d888e5c2759813a4d8fe401eeb3e02deef539e6
Dan Trickey <dan.trickey@torchbox.com>
2025-09-24 14:24:43 +0100

    feat: add template and context to view

--- newscrawler/news/views.py
+++ newscrawler/news/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
 
+from django.utils import timezone
+from datetime import timedelta
+
+from .models import NewsArticle
+
 
 def index(request):
-    return render(request, "news/index.html")
+    news_articles = NewsArticle.objects.order_by("-crawl_date").filter(
+        article_date__gte=timezone.now() - timedelta(days=7)
+    )
+
+    return render(
+        request,
+        "news/index.html",
+        context={
+            "news_articles": news_articles,
+        },
+    )
